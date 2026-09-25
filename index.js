0dde18a8e4432ed002679e2fa4b4cb50e3af4646
AmannRawat <amanrawat1509@gmail.com>
2026-09-25 17:11:07 +0530

    Project finished

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import { GoogleGenAI } from "@google/genai";
 import "dotenv/config";
 import axios from "axios";
 import * as cheerio from "cheerio";
+import puppeteer from "puppeteer";
+import readlineSync from "readline-sync";
 import { ChromaClient } from "chromadb";
 
 // dotenv.config();
@@ -19,13 +21,35 @@ chromadbClient.heartbeat();
 
 const WEB_COLLECTION = 'WEB_SCRAPED_DATA_COLLECTION-1'
 
+const visitedUrls = new Set();
+function normalizeUrl(url) {
+    const u = new URL(url);
+
+    // Remove hash (#about)
+    u.hash = "";
+
+    // Remove trailing slash except root
+    if (u.pathname !== "/" && u.pathname.endsWith("/")) {
+        u.pathname = u.pathname.slice(0, -1);
+    }
+
+    return u.href;
+}
+
+//Delete the collection if already exists only for development phase
+try {
+    await chromadbClient.deleteCollection({
+        name: WEB_COLLECTION,
+    });
+} catch { }
+
 const collection = await chromadbClient.getOrCreateCollection({
     name: WEB_COLLECTION,
 });
 
 
-
-async function scrapeWebpage(url = "") {
+//This function is only for server side rendering webpages!
+async function scrapeStaticWebsite(url = "") {
     // Static websites → Axios
     const { data } = await axios.get(url);
 
@@ -47,6 +71,64 @@ async function scrapeWebpage(url = "") {
     })
     return { head: pageHead, body: pageBody, internalLinks, externalLinks };
 }
+//This function for client side rendering web pages like React Vite
+async function scrapeReactWebsite(url = "") {
+    const browser = await puppeteer.launch({
+        headless: true,
+    });
+
+    const page = await browser.newPage();
+
+    await page.goto(url, {
+        waitUntil: "networkidle2",
+    });
+
+    const html = await page.content();
+    const $ = cheerio.load(html);
+
+    const pageHead = $("head").html();
+
+    const pageBody = await page.evaluate(() =>
+        document.body.innerText.replace(/\s+/g, " ").trim()
+    );
+
+    // console.log("Characters:", pageBody.length);
+    // console.log("Preview:", pageBody.slice(0, 300));
+
+    await browser.close();
+
+    const internalLinks = [];
+    const externalLinks = [];
+
+    $("a").each((_, element) => {
+        const link = $(element).attr("href");
+
+        if (!link) return;
+
+        // Skip anchors, email, phone and JS links
+        if (
+            link.startsWith("#") ||
+            link.startsWith("mailto:") ||
+            link.startsWith("tel:") ||
+            link.startsWith("javascript:")
+        ) {
+            return;
+        }
+
+        if (link.startsWith("http")) {
+            externalLinks.push(link);
+        } else {
+            internalLinks.push(link);
+        }
+    });
+
+    return {
+        head: pageHead,
+        body: pageBody,
+        internalLinks,
+        externalLinks,
+    };
+}
 
 async function generateVectorEmbeddings({ text }) {
     const response = await ai.models.embedContent({
@@ -55,13 +137,16 @@ async function generateVectorEmbeddings({ text }) {
         encoding_format: 'FLOAT',
     });
     // Ptinting Embedding
-    console.log("Dimension:", response.embeddings[0].values.length);
-    console.log("First 5 values:", response.embeddings[0].values.slice(0, 5));
+    // console.log("Dimension:", response.embeddings[0].values.length);
+    // console.log("First 5 values:", response.embeddings[0].values.slice(0, 5));
     return response.embeddings[0].values;
 }
 
 async function ingest(url = '') {
-    const { head, body, internalLinks } = await scrapeWebpage(url);
+    url = normalizeUrl(url);
+    if (visitedUrls.has(url)) return;
+    visitedUrls.add(url);
+    const { head, body, internalLinks } = await scrapeReactWebsite(url);
     const headEmbedding = await generateVectorEmbeddings({ text: head });
 
     await insertIntoDb({
@@ -73,7 +158,9 @@ async function ingest(url = '') {
     });
 
     const bodyChunks = chunkText(body); // Made chunks because context window size is limited for embedding content and body was lengthy
+
     for (const [chunkIndex, chunk] of bodyChunks.entries()) {
+        if (!chunk.trim()) continue;
         const bodyEmbedding = await generateVectorEmbeddings({ text: chunk });
         await insertIntoDb({
             id: `${url}-${chunkIndex}`,
@@ -85,9 +172,10 @@ async function ingest(url = '') {
     }
 
     for (const link of internalLinks) {
-        const _url = `${url}${link}`;
+        const _url = normalizeUrl(new URL(link, url).href);
         await ingest(_url);
     }
+    console.log("Crawling:", url);
 }
 
 async function insertIntoDb({ id, embedding, url, body = '', head = '' }) {
@@ -118,7 +206,8 @@ export function chunkText(text, chunkSize = 300) {
     return chunks;
 }
 
-await ingest('https://prepai-app.vercel.app/');
+console.log("🚀 Starting ingestion...");
+await ingest('https://anupbh77.github.io');
 
 async function chat(question = '') {
     const questionEmbedding = await generateVectorEmbeddings({ text: question });
@@ -128,7 +217,7 @@ async function chat(question = '') {
         include: ['metadatas', 'documents']
     })
 
-    console.log("Retrieved:", JSON.stringify(collectionResult, null, 2));
+    // console.log("Retrieved:", JSON.stringify(collectionResult, null, 2));
 
     const body = collectionResult.documents[0]
         .filter((e) => e && e.trim() !== "");
@@ -153,10 +242,26 @@ async function chat(question = '') {
 `
     });
 
-    console.log({
-        message: `🤖 AI Support Agent: ${response.text}`,
-        SourceURL: url[0],
-    });
+    console.log("\n" + "═".repeat(60));
+    console.log("🤖  AI SUPPORT AGENT");
+    console.log("═".repeat(60));
+
+    console.log("\n" + response.text);
+
+    console.log("\n🔗 Source:", url[0]);
+    console.log("═".repeat(60));
 }
 
-await chat("What PrepAi App Does?");
+// await chat("What this App Does?");
+// await chat("Tell me about Anup who is he what does he do?");
+
+while (true) {
+    const question = readlineSync.question("\n💭 You: ");
+
+    if (question.toLowerCase() === "exit") {
+        console.log("\n👋 Goodbye!");
+        break;
+    }
+
+    await chat(question);
+}
